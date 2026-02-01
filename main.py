ffac845bf40535b13347aa29018b5e8370708d2a
btallon1 <brycetallon@gmail.com>
2026-02-01 14:42:27 -0500

    basic chatbot setup

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("No valid API key found")
+    client = genai.Client(api_key=api_key)
+    
+    parser = argparse.ArgumentParser(description="AI Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    verbose_flag = args.verbose
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+        )
+    if response.usage_metadata == None:
+        raise RuntimeError("usage_metadata == None. API request likely failed")
+    elif verbose_flag == True:
+        print(f"User prompt: {args.user_prompt}")
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    
+    print(response.text)
+
+
+
+if __name__ == "__main__":
+    main()
